7eb3df99f5345618e0a327690d65f3cb77d0b311
ayushhkkkk3585 <ayushkhalate04@gmail.com>
2025-01-23 22:37:23 +0530

    favicon changes

--- src/components/Animated.jsx
+++ src/components/Animated.jsx
@@ -4,33 +4,35 @@ import AnimatedCursor from "react-animated-cursor";
 const Animated = () => {
   return (
     <>
-      <AnimatedCursor
-        innerSize={12}
-        outerSize={0}
-        innerScale={1.7}
-        outerScale={1.7}
-        outerAlpha={0}
-        hasBlendMode={true}
-        innerStyle={{
-          backgroundColor: "white",
-        }}
-        outerStyle={{
-          border: "3px solid white",
-        }}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-        ]}
-      />
+      <div className="hidden lg:block">
+        <AnimatedCursor
+          innerSize={12}
+          outerSize={0}
+          innerScale={1.7}
+          outerScale={1.7}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: "white",
+          }}
+          outerStyle={{
+            border: "3px solid white",
+          }}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
+        />
+      </div>
     </>
   );
 };
